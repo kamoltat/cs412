2f2749a8e999a7ec1019aad5664cee8e08e522ee
Kamoltat Sirivadhna <ksirivad@bu.edu>
2020-06-09 16:53:16 -0400

    ps3 completed

---
+++ PS3/routes/ps3.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'PS3', message: '1. Try type something in the url for example: http://localhost:3000/hello \n' + '2. Try parsing something using Postman' })
+});
+
+//This is solution to 1b since it can be anything, you simply just type anything and it iwll render that string
+// to Pug template
+router.get('/:id', function(req, res, next) {
+    res.render('get', { string:req.params.id})
+});
+
+router.get('/:id', function(req, res, next) {
+    res.render('get', { string:req.params.id})
+});
+
+router.post('/', function(req, res, next) {
+    let id = req.body.string;
+    res.render('post', {string:id, length: id.length})
+});
+
+
+module.exports = router;
