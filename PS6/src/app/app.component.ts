75392a251f293a14a79b2d7e77e41fb8bf73a871
Kamoltat Sirivadhna <ksirivad@bu.edu>
2020-06-27 09:15:06 +0700

    PS6 submit

---
+++ PS6/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {COUNTRIES} from './data/countryMOCK';
+import {COUNTRY} from './data/country';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  countries: COUNTRY[] = COUNTRIES;
+  title = 'PS6';
+  selectedCountry: COUNTRY;
+
+  showStats(country: string) {
+    this.selectedCountry  = this.countries.find(theCountry => theCountry.Country === country);
+  }
+
+}
---
+++ PS6/src/app/data/countryMOCK.ts
@@ -0,0 +1,29 @@
+import {COUNTRY} from './country';
+
+export const COUNTRIES:COUNTRY[] = 
+    [
+        {
+        Country: 'Thailand',
+        CountryCode: 'TH',
+        Slug: 'thailand',
+        NewConfirmed: 5,
+        TotalConfirmed: 3156,
+        NewDeaths: 0,
+        TotalDeaths: 58,
+        NewRecovered: 1,
+        TotalRecovered: 3023,
+        Date: '2020-06-24T19:39:41Z'
+      },
+      {
+        Country: "Korea (South)",
+        CountryCode: "KR",
+        Slug: "korea-south",
+        NewConfirmed: 94,
+        TotalConfirmed: 10156,
+        NewDeaths: 3,
+        TotalDeaths: 177,
+        NewRecovered: 304,
+        TotalRecovered: 6325,
+        Date: "2020-04-05T06:37:00Z"
+      }
+    ]
